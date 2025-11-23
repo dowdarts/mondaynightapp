3dd0aa66f2835891c3ad2518582e089ce40c048e
dow1800 <dow1800@gmail.com>
2025-11-23 17:30:34 -0400

    Add SQL migration to fix match_history table schema

---
+++ fix-match-history-schema.sql
@@ -0,0 +1,13 @@
+-- Fix match_history table schema to add missing user_name column
+ALTER TABLE match_history 
+ADD COLUMN IF NOT EXISTS user_name TEXT;
+
+-- Update existing records to set user_name if missing
+-- This extracts from session_id or sets to 'Unknown User'
+UPDATE match_history 
+SET user_name = 'Unknown User' 
+WHERE user_name IS NULL;
+
+-- Make user_name NOT NULL after setting defaults
+ALTER TABLE match_history 
+ALTER COLUMN user_name SET NOT NULL;
